5731ee75816ede0a29aa376e0a5ac10712079d1b
liuyang <yang.liu06@luckincoffee.com>
2018-11-28 15:32:03 +0800

    Site updated: 2018-11-28 15:32:03

--- js/util.js
+++ js/util.js
@@ -1,66 +1,30 @@
-const Marked = window.marked;
+function hasMobileUA () {
+    var nav = window.navigator;
+    var ua = nav.userAgent;
+    var pa = /iPad|iPhone|Android|Opera Mini|BlackBerry|webOS|UCWEB|Blazer|PSP|IEMobile|Symbian/g;
 
-// Marked.setOptions({
-//   highlight: (code, lang) =>
-//     Prism.highlight(code, Prism.languages[lang || "markup"], lang || "markup")
-// });
+    return pa.test(ua);
+ }
 
-const mdToHTML = content => Marked(content)
-
-const getRealPath = (pathname, desc = false) => {
-  if(!pathname) {
-    pathname = window.location.pathname
-  }
-  let names = pathname.split("/")
-  if(desc === false) {
-    for(let i = names.length - 1; i >= 0; --i) {
-      let name = names[i].trim()
-      if(name.length > 0 && name !== "/" && name !== "index.html") {
-        return name
-      }
-    }
-  } else {
-    for(let i = 0 ; i < names.length; ++i) {
-      let name = names[i].trim()
-      if(name.length > 0 && name !== "/" && name !== "index.html") {
-        return name
-      }
-    }
-  }
-  return "/"
+function isMobile() {
+    return window.screen.width < 767 && hasMobileUA();
 }
 
-const generateToc = () => {
-  if(document.body.clientWidth < 768) {
-    return;
-  }
-  $("#sidebar-header").append("<span> Table of Contents </span>");
-  let sidebar = $("#sidebar"),
-    app = $("#app"),
-    topBtn = $(".back-to-top");
-  app.addClass("sidebar-active");
-  sidebar.addClass("sidebar-active");
-  if (document.body.clientWidth <= 768) {
-    topBtn.attr("style", "right: calc(2rem + 200px);");
-  } else {
-    topBtn.attr("style", "right: calc(2rem + 250px);");
-  }
-  topBtn.addClass("sidebar-active");
-  
-  $(".markdown-body")
-    .find("h2,h3,h4,h5,h6")
-    .each(function(i, item) {
-      let tag = $(item).get(0).localName;
-      let tagID = $(item)
-        .text()
-        .replace(/\s{2}/g, "");
-      $(item).attr("id", tagID);
-      $("#sidebar-toc").append(`
-      <li class="toc-${tag}">
-        <a href="#${tagID}" target="_self">
-          ${tagID}
-        </a>
-      </li>
-    `);
-    });
+function loadjscssfile(filename,filetype){
+
+    if(filetype == "js"){
+        var fileref = document.createElement('script');
+        fileref.setAttribute("type","text/javascript");
+        fileref.setAttribute("src",filename);
+    }else if(filetype == "css"){
+    
+        var fileref = document.createElement('link');
+        fileref.setAttribute("rel","stylesheet");
+        fileref.setAttribute("type","text/css");
+        fileref.setAttribute("href",filename);
+    }
+   if(typeof fileref != "undefined"){
+        document.getElementsByTagName("head")[0].appendChild(fileref);
+    }
+    
 }
